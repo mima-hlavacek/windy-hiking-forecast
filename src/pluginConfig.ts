6d0736473c3ccec61598d08da1795662cf3ecdcc
Michael Hlaváček <michael.hlavacek@windy.com>
2026-08-04 13:40:46 +0200

    Bump version.

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-hiking-forecast',
-    version: '0.1.2',
+    version: '0.1.3',
     icon: '🥾',
     title: 'Hiking Forecast',
     description: 'Combined forecast showing temperature, wind, clouds, and rain.',
